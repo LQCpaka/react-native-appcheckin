500ae5cc69c3c48440b15fe411bef5279a14a587
LQCpaka <boyrovip1539@gmail.com>
2025-06-16 21:22:53 +0700

    components update

---
+++ components/ItemInventoryList.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const ItemInventoryList = () => {
+  return (
+    <View>
+      <Text>Inventory List</Text>
+    </View>
+  )
+}
+
+export default ItemInventoryList
